98035fcd878dc761c8cdc636f92a7a450c2c5912
Soldann <guotl321@gmail.com>
2020-10-09 23:05:42 -0400

    fix formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,91 +1,89 @@
 navigator.getUserMedia(
-    { video: true, audio: true },
-    stream => {
-      const localVideo = document.getElementById("local-video");
-      if (localVideo) {
-        localVideo.srcObject = stream;
-      }
-    },
-    error => {
-      console.warn(error.message);
+  { video: true, audio: true },
+  (stream) => {
+    const localVideo = document.getElementById("local-video");
+    if (localVideo) {
+      localVideo.srcObject = stream;
     }
-   );
+  },
+  (error) => {
+    console.warn(error.message);
+  }
+);
 
-   var socket = io();
- 
+var socket = io();
+
 socket.on("update-user-list", ({ users }) => {
   updateUserList(users);
   console.log("hello workd");
- });
-  
- socket.on("remove-user", ({ socketId }) => {
+});
+
+socket.on("remove-user", ({ socketId }) => {
   const elToRemove = document.getElementById(socketId);
   console.log("remove user");
   if (elToRemove) {
     elToRemove.remove();
   }
- });
+});
 
- function updateUserList(socketIds) {
+function updateUserList(socketIds) {
   const activeUserContainer = document.getElementById("active-user-container");
-  
-  socketIds.forEach(socketId => {
+
+  socketIds.forEach((socketId) => {
     const alreadyExistingUser = document.getElementById(socketId);
     if (!alreadyExistingUser) {
       const userContainerEl = createUserItemContainer(socketId);
       activeUserContainer.appendChild(userContainerEl);
     }
   });
- }
+}
 
- function createUserItemContainer(socketId) {
+function createUserItemContainer(socketId) {
   const userContainerEl = document.createElement("div");
-  
+
   const usernameEl = document.createElement("p");
-  
+
   userContainerEl.setAttribute("class", "active-user");
   userContainerEl.setAttribute("id", socketId);
   usernameEl.setAttribute("class", "username");
   usernameEl.innerHTML = `Socket: ${socketId}`;
-  
+
   userContainerEl.appendChild(usernameEl);
-  
+
   userContainerEl.addEventListener("click", () => {
     //unselectUsersFromList();
     userContainerEl.setAttribute("class", "active-user active-user--selected");
     const talkingWithInfo = document.getElementById("talking-with-info");
     talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
     callUser(socketId);
-  }); 
+  });
   return userContainerEl;
- }
+}
 
- 
- const { RTCPeerConnection, RTCSessionDescription } = window;
- var peerConnection = new RTCPeerConnection();
- 
+const { RTCPeerConnection, RTCSessionDescription } = window;
+var peerConnection = new RTCPeerConnection();
+
 async function callUser(socketId) {
-  
   //RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection;
 
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
-  
+
   socket.emit("call-user", {
     offer,
-    to: socketId
+    to: socketId,
   });
- }
+}
 
- socket.on("call-made", async data => {
+socket.on("call-made", async (data) => {
   await peerConnection.setRemoteDescription(
     new RTCSessionDescription(data.offer)
   );
   const answer = await peerConnection.createAnswer();
   await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
-  
+
   socket.emit("make-answer", {
     answer,
-    to: data.socket
+    to: data.socket,
   });
- });
+});
--- src/server.ts
+++ src/server.ts
@@ -2,78 +2,85 @@ import express, { Application } from "express";
 import socketIO, { Server as SocketIOServer } from "socket.io";
 import { createServer, Server as HTTPServer } from "http";
 import path from "path";
- 
+
 export class Server {
- private httpServer: HTTPServer;
- private app: Application;
- private io: SocketIOServer;
- private activeSockets: string[] = [];
- 
- private readonly DEFAULT_PORT = 80;
- 
- constructor() {
-   this.app = express();
-   this.httpServer = createServer(this.app);
-   this.io = socketIO(this.httpServer);
-   this.app.use(express.static(path.join(__dirname, "./pages")));
- 
-   this.handleRoutes();
-   this.handleSocketConnection();
- }
- 
- private handleRoutes(): void {
-   this.app.get("/", (req, res) => {
-     res.send(`<h1>Hello World</h1>`); 
-   });
- }
- 
- private handleSocketConnection(): void {
-   this.io.on("connection", socket => {
-     console.log("Socket connected.");
+  private httpServer: HTTPServer;
+  private app: Application;
+  private io: SocketIOServer;
+  private activeSockets: string[] = [];
 
-     console.log("Connection detected. Before: " + this.activeSockets);
+  private readonly DEFAULT_PORT = 80;
 
-     const existingSocket = this.activeSockets.find(
-      existingSocket => existingSocket === socket.id
+  constructor() {
+    this.app = express();
+    this.httpServer = createServer(this.app);
+    this.io = socketIO(this.httpServer);
+    this.app.use(express.static(path.join(__dirname, "./pages")));
+
+    this.handleRoutes();
+    this.handleSocketConnection();
+  }
+
+  private handleRoutes(): void {
+    this.app.get("/", (req, res) => {
+      res.send(`<h1>Hello World</h1>`);
+    });
+  }
+
+  private handleSocketConnection(): void {
+    this.io.on("connection", (socket) => {
+      console.log("Socket connected.");
+
+      console.log("Connection detected. Before: " + this.activeSockets);
+
+      const existingSocket = this.activeSockets.find(
+        (existingSocket) => existingSocket === socket.id
+      );
+
+      if (!existingSocket) {
+        this.activeSockets.push(socket.id);
+
+        socket.emit("update-user-list", {
+          users: this.activeSockets,
+        });
+
+        socket.broadcast.emit("update-user-list", {
+          users: [socket.id],
+        });
+
+        socket.on("disconnect", () => {
+          this.activeSockets = this.activeSockets.filter(
+            (existingSocket) => existingSocket !== socket.id
+          );
+          socket.broadcast.emit("remove-user", {
+            socketId: socket.id,
+          });
+          console.log("Connection removed: " + this.activeSockets);
+        });
+
+        socket.on("call-user", (data) => {
+          console.log("sending call");
+          socket.to(data.to).emit("call-made", {
+            offer: data.offer,
+            socket: socket.id,
+          });
+        });
+
+        socket.on("make-answer", (data) => {
+          socket.to(data.to).emit("answer-made", {
+            socket: socket.id,
+            answer: data.answer,
+          });
+        });
+      }
+
+      console.log("Connection detected. After: " + this.activeSockets);
+    });
+  }
+
+  public listen(callback: (port: number) => void): void {
+    this.httpServer.listen(this.DEFAULT_PORT, () =>
+      callback(this.DEFAULT_PORT)
     );
-
-    if (!existingSocket) {
-      this.activeSockets.push(socket.id);
-
-      socket.emit("update-user-list", {
-        users: this.activeSockets
-      });
-
-      socket.broadcast.emit("update-user-list", {
-        users: [socket.id]
-      });
-
-      socket.on("disconnect", () => {
-        this.activeSockets = this.activeSockets.filter(
-          existingSocket => existingSocket !== socket.id
-        );
-        socket.broadcast.emit("remove-user", {
-          socketId: socket.id
-        });
-        console.log("Connection removed: " + this.activeSockets);
-      });
-
-      socket.on("call-user", data => {
-        console.log("sending call");
-        socket.to(data.to).emit("call-made", {
-          offer: data.offer,
-          socket: socket.id
-        });
-      });
-    }
-
-    console.log("Connection detected. After: " + this.activeSockets);
-   });
- }
- 
- public listen(callback: (port: number) => void): void {
-   this.httpServer.listen(this.DEFAULT_PORT, () =>
-     callback(this.DEFAULT_PORT)
-   );
- }
+  }
 }
