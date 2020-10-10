d969cb7b9a6b7b66ad57131565c86fcdf635d972
Ben <li.benjamin.b@gmail.com>
2020-10-10 16:55:16 -0400

    Added buttons and text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple. Fast. Secure.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button> Make room</button>
+          <button> Join room</button>
+        </div>
       </header>
     </div>
   );
