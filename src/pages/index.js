3e39f274bed1f45a298a2665306b5c8e88d4701d
Soldann <guotl321@gmail.com>
2020-10-10 20:42:41 -0400

    add twilio stun server

--- src/pages/index.js
+++ src/pages/index.js
@@ -107,7 +107,7 @@ const { RTCPeerConnection, RTCSessionDescription } = window;
 var peerConnection = new RTCPeerConnection({
   iceServers: [     // Information about ICE servers - Use your own!
     { urls: 'stun:stun.l.google.com:19302' },
-   // { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
   //  { urls: 'stun:stun2.l.google.com:19302' },
    // { urls: 'stun:stun3.l.google.com:19302' },
    // { urls: 'stun:stun4.l.google.com:19302' }
